78ded3a28c09736938f281a4c7297c780f6067c3
Kristina Et cetera <isitiriss@gmail.com>
2016-10-29 19:33:01 +0300

    Update scrot-imgur.sh

--- scrot-imgur.sh
+++ scrot-imgur.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-function uploadImage {
-    curl -s -F "image=@$1" -F "key=486690f872c678126a2c09a9e196ce1b" https://imgur.com/api/upload.xml | grep -E -o "<original_image>(.)*</original_image>" | grep -E -o "http://i.imgur.com/[^<]*"
-}
+
 sleep 0.2
-scrot -s "/tmp/shot.png"
-uploadImage "/tmp/shot.png" | xclip -selection clipboard
-notify-send --icon="accessories-camera" "Scrotted to imgur" "You can paste the link now"
-if [ -f /tmp/shot.png ]
-then
-    rm "/tmp/shot.png"
+scrot -s "shot.png" 
+imgur.sh "shot.png" 
+if [ "$?" -ne 0 ]
+    then
+    notify-send "Fail :("
+	rm "shot.png"
+    exit 1                
 fi
+rm "shot.png"
+notify-send IMGUR "Done"
 # binded to Super+i by default
